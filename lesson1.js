db2c11ab2ab5fbb7e1046ad24fb0fb58f6b8c0e0
unknown <dianahoveyan@gmail.com>
2020-09-17 14:01:56 +0400

    first commit

---
+++ lesson1.js
@@ -0,0 +1,9 @@
+
+const PI = Math.PI
+let r = parseFloat(prompt("Please enter the radius of a circle"));
+console.log(r)
+let a = PI * r * r
+let p = 2 * PI * r
+alert("The area of circle is " + a); 
+alert("The perimeter of circle is " + p);
+ 
