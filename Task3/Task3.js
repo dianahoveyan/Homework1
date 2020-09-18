5142358cf45e899d05b02f1849e3c50c460456cf
unknown <dianahoveyan@gmail.com>
2020-09-19 01:43:18 +0400

    task3

---
+++ Task3/Task3.js
@@ -0,0 +1,53 @@
+var mytext = parseFloat(prompt("Enter the text with min 5 symbols"));
+var number = 0
+if (mytext.length < 5){
+    alert ("Try again with less symbols")
+}
+else
+{
+    switch (mytext.charAt(0)){
+        case "a":
+        case "e":
+        case "u":
+        case "i":
+        case "o":
+            number += 4
+            break;
+        case "z":
+            number += 5
+            break;
+         default:
+            number -= 1   
+    }
+    switch (mytext.charAt(1)){
+        case "a":
+        case "e":
+        case "u":
+        case "i":
+        case "o":
+            number += 4
+            break;
+        case "z":
+            number += 5
+            break;
+        default:
+            number -= 1
+    }
+    switch (mytext.charAt(2)){
+        case "a":
+        case "e":
+        case "u":
+        case "i":
+        case "o":
+            number += 4
+            break;
+        case "z":
+            number += 5
+            break;
+        default:
+            number -= 1
+    }
+    alert ("Your number is " + number);
+
+}  
+            
